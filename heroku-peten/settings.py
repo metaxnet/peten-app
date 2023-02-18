1f754b5419f6d3b2357730455c97eababab41376
Eli <metaxnet@gmail.com>
2023-02-18 08:55:59 +0200

    running

--- heroku-peten/settings.py
+++ heroku-peten/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['petenapp-metaxnet.b4a.run']
 
 
 # Application definition
